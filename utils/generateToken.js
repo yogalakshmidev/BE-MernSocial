db2c6a2f2363a937c2cc0e393e9565854b24a58f
Yogalakshmi <yogarajakanibe@gmail.com>
2025-04-30 04:39:10 +0530

    cookie time changes

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -6,18 +6,18 @@ const generateToken = (id, res) => {
   });
 
   
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true, // set to true in production
-    sameSite: "None", // if using cross-origin
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  });
-
   // res.cookie("token", token, {
-  //   maxAge: 15 * 24 * 60 * 60 * 1000,
   //   httpOnly: true,
-  //   sameSite: "strict",
+  //   secure: true, // set to true in production
+  //   sameSite: "None", // if using cross-origin
+  //   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   // });
+
+  res.cookie("token", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "None",
+  });
 };
 
 export default generateToken;
